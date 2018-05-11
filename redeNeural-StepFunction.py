83e72d1e137622a150a44c5ad84ca278b34bf9bb
Rafael Fronteira <rafael_fronteira@hotmail.com>
2018-05-11 16:19:23 -0300

    first AI code

---
+++ redeNeural-StepFunction.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+import numpy as np
+
+
+entradas= np.array([ [0,0], [0,1], [1,0], [1,1]])
+saidas = np.array([0,0,0,1])
+pesos = np.array([0.0, 0.0])
+taxaAprendizagem = 0.1
+
+
+def stepFunction(soma):
+    if soma >= 1:
+        return 1
+    return 0
+
+
+def calculaSaida(registro):
+    s = registro.dot(pesos)
+    return stepFunction(s)
+
+
+def treinar():
+    erroTotal = 1
+    while erroTotal != 0:
+        erroTotal = 0
+        for i in range(len(saidas)):
+            saidaCalculada = calculaSaida(np.asarray(entradas[i]))
+            erro = abs(saidas[i] - saidaCalculada)
+            erroTotal += erro
+            for j in range(len(pesos)):
+                pesos[j] = pesos[j] + (taxaAprendizagem * entradas[i][j] * erro)
+                print('Peso Atualizado: {}'.format(pesos[j]))
+        print('Print total de erros: {}'.format(erroTotal))
+
+
+treinar()
+print('Rede neural treinada')
+print(calculaSaida(entradas[0]))
+print(calculaSaida(entradas[1]))
+print(calculaSaida(entradas[2]))
+print(calculaSaida(entradas[3]))
